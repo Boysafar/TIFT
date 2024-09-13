672b6c86bb73ce33edfa574d9d7fa5ea48de02be
Boysafar <boysafarnurmatov@gmail.com>
2024-09-13 14:54:44 +0500

    initial commit

--- core/settings.py
+++ core/settings.py
@@ -131,12 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Using Path objects if you are using Django 4.0 or later
-    # or
-    os.path.join(BASE_DIR, "static"),  # Using string paths
-]
-
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
